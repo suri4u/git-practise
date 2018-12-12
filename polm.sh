cba95e8f583c3375b5c7f1b42171c7d599b11cbf
root <you@example.com>
2018-12-12 22:10:10 +0530

    second commit

---
+++ polm.sh
@@ -0,0 +1,11 @@
+E4. Delete the following text: *Delete this line to make a change to the README from Bitbucket.*
+5. After making your change, click **Commit** and then **Commit** again in the dialog. The commit page will open and you’ll see the change you just made.
+6. Go back to the **Source** page.
+
+---
+u entered in to remote repository
+thank you polm.
+second file to git
+
+
+
