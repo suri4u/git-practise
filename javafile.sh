526d6424c13ea9e9d8bc849001840e1da17ac834
git-suri <45818618+git-suri@users.noreply.github.com>
2018-12-18 20:13:16 +0530

    Update javafile.sh

--- javafile.sh
+++ javafile.sh
@@ -8,7 +8,7 @@
 2. Click the README.md link from the list of files.
 3. Click the **Edit** button.
 4. Delete the following text: *Delete this line to make a change to the README from Bitbucket.*
-5. After making your change, click **Commit** and then **Commit** again in the dialog. The commit page will open and you’ll see the change you just made
+5. After making your change, click **Commit** and then **Commit** again in the dialog. The commit page will open and youâ€™ll see the change you just made
 .
 6. Go back to the **Source** page.
 7.
@@ -17,4 +17,4 @@
 10.
 
 thank you very much..!!!!!
-
+abbo...!!!
